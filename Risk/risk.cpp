3f15ed03f7ea852cdd178fba8260926751d516c7
Dante <eshock7@gmail.com>
2018-02-22 21:48:00 -0500

    added simulation

--- Risk/risk.cpp
+++ Risk/risk.cpp
@@ -5,36 +5,18 @@
 #include<cstdlib>
 using namespace std;
 
-// attack:
-//   ask player for # of soldiers 
-//   roll #of soldier<=3 dice
-//   calculate player loss
-
-// battle_over:
-//   attacker_count <2  or defender_count == 0
-
-// battle_win:
-//     defender_count <= 0
-
-// sim_battle:
-//   store player count
-//   do call attack
-//   until battle_over
-//   restore player count
-//   return battle_win
-
-// run_full_sim:
-//   for i = 1 to 10000
-//     call sim_battle
-//     tally win/lose
-//   print wins divided by 10000 as %
-
+void initdice();
 void realbattle();
-void attack(int *ptr_attacksoldiers, int *ptr_defendsoldiers);
-void attackrolls(int attackernumrolls, int defendernumrolls, int *ptr_attacklosses, int *ptr_defendlosses);
+int runfullsim(int attacksoldiers, int defendsoldiers);
+bool simbattle(int attacksoldiers, int defendsoldiers);
+void attack(int *ptr_attacksoldiers, int *ptr_defendsoldiers,bool print);
+void attackrolls(int attackernumrolls, int defendernumrolls, int *ptr_attacklosses, int *ptr_defendlosses, bool print);
 bool battleover(int attacksoldiers, int defendsoldiers);
+int rolldice();
+void swap(int array[], int x, int y);
 
 int main(){
+	initdice();
 	realbattle();
 	string temp;
 	cin >> temp;
@@ -44,15 +26,6 @@ int main(){
 // Dice and sorting utility functions.
 void initdice(){
 	srand(unsigned(time(NULL)));
-}
-int rolldice(){
-	return (rand() % 6) + 1;
-}
-void swap(int array[], int x, int y){
-	int temp;
-	temp = array[x];
-	array[x] = array[y];
-	array[y] = temp;
 }
 
 // real_battle:
@@ -82,18 +55,21 @@ void realbattle(){
 			cin.ignore(80, '\n');
 		}
 	}
+	
 	do{
 		string response = "";
 		cout << "There are " << attacksoldiers << " attacking.\n";
 		cout << "There are " << defendsoldiers << " defending.\n";
-		//TODO: RUN SIMULATION
+		//RUN SIMULATION
+		int percentwins = runfullsim(attacksoldiers, defendsoldiers);
+		cout<< "You have a "<<percentwins<<"% chance of winning the battle.\n";
 		while (response != "y" && response != "n"){
 			cout << "Do you want to attack?(y/n) ";
 			cin >> response;
 		}
 		if (response == "n")
 			break;
-		attack(&attacksoldiers, &defendsoldiers);
+		attack(&attacksoldiers, &defendsoldiers,true);
 	} while (!battleover(attacksoldiers, defendsoldiers));
 	cout << "The battle is over!  Attacker has " << attacksoldiers << ". Defender has " << defendsoldiers << ".\n";
 	if (defendsoldiers == 0)
@@ -102,13 +78,43 @@ void realbattle(){
 		cout << "Defender wins!\n";
 }
 
+// run_full_sim:
+//   for i = 1 to 10000
+//     call sim_battle
+//     tally win/lose
+//   print wins divided by 10000 as %
+int runfullsim(int attacksoldiers, int defendsoldiers){
+	int wins=0;
+	for (int i = 0; i < 100000; i++){
+		if (simbattle(attacksoldiers, defendsoldiers))
+			wins++;
+	}
+	return 100*wins / 100000;
+}
+// sim_battle:
+//   store player count
+//   do call attack
+//   until battle_over
+//   restore player count
+//   return battle_win
+bool simbattle(int attacksoldiers, int defendsoldiers){
+	do{
+		attack(&attacksoldiers, &defendsoldiers,false);
+	} while (!battleover(attacksoldiers, defendsoldiers));
+	if (defendsoldiers == 0)
+		return true;
+	else
+		return false;
+}
+
 // Roll dice.  Deduct soldiers by loss counts.
-void attack(int *ptr_attacksoldiers, int *ptr_defendsoldiers){
+void attack(int *ptr_attacksoldiers, int *ptr_defendsoldiers,bool print){
 	int attacksoldiers = *ptr_attacksoldiers;
 	int defendsoldiers = *ptr_defendsoldiers;
 	int attacklosses, defendlosses;
-	attackrolls(attacksoldiers - 1, defendsoldiers, &attacklosses, &defendlosses);
-	cout << "Attacker lost " << attacklosses << " soldiers and defender lost " << defendlosses << " soldiers.\n";
+	attackrolls(attacksoldiers - 1, defendsoldiers, &attacklosses, &defendlosses,print);
+	if (print)
+		cout << "Attacker lost " << attacklosses << " soldiers and defender lost " << defendlosses << " soldiers.\n";
 	attacksoldiers -= attacklosses;
 	defendsoldiers -= defendlosses;
 	*ptr_attacksoldiers = attacksoldiers;
@@ -118,7 +124,7 @@ void attack(int *ptr_attacksoldiers, int *ptr_defendsoldiers){
 // Take the number of soldiers who can attack or defend.  Limit 3.  Roll dice and sort descending.
 // Compare pairs of dice.  Attacker must have greater to win.  Tie goes to defender.
 // Count losses.
-void attackrolls(int attackernumrolls,int defendernumrolls, int *ptr_attacklosses, int *ptr_defendlosses){
+void attackrolls(int attackernumrolls,int defendernumrolls, int *ptr_attacklosses, int *ptr_defendlosses,bool print){
 	const int maxrolls = 3;
 	int attacker[maxrolls];
 	int defender[maxrolls];
@@ -135,7 +141,6 @@ void attackrolls(int attackernumrolls,int defendernumrolls, int *ptr_attacklosse
 	else
 		usablerolls = defendernumrolls;
 
-	initdice();
 	for (int i = 0; i < attackernumrolls; i++)
 		attacker[i] = rolldice();
 	for (int i = 0; i < defendernumrolls; i++)
@@ -155,18 +160,18 @@ void attackrolls(int attackernumrolls,int defendernumrolls, int *ptr_attacklosse
 			}
 		}
 	}
-
-	cout << "Attacker rolled";
-	for (int i = 0; i < attackernumrolls; i++) {
-		cout << " " << attacker[i];
+	if (print){
+		cout << "Attacker rolled";
+		for (int i = 0; i < attackernumrolls; i++) {
+			cout << " " << attacker[i];
+		}
+		cout << ".\n";
+		cout << "Defender rolled";
+		for (int i = 0; i < defendernumrolls; i++) {
+			cout << " " << defender[i];
+		}
+		cout << ".\n";
 	}
-	cout << ".\n";
-	cout << "Defender rolled";
-	for (int i = 0; i < defendernumrolls; i++) {
-		cout << " " << defender[i];
-	}
-	cout << ".\n";
-
 	for (int i = 0; i < usablerolls; i++) {
 		if (attacker[i] > defender[i])
 			defenderlosses++;
@@ -178,9 +183,22 @@ void attackrolls(int attackernumrolls,int defendernumrolls, int *ptr_attacklosse
 }
 
 // Take the number of soldiers for each side.  Battle isn't over until attacker has 1 or defender has 0.
+// battle_over:
+//   attacker_count <2  or defender_count == 0
 bool battleover(int attacksoldiers, int defendsoldiers){
 	if (attacksoldiers < 2 || defendsoldiers == 0)
 		return true;
 	else
 		return false;
+}
+
+int rolldice(){
+	return (rand() % 6) + 1;
+}
+
+void swap(int array[], int x, int y){
+	int temp;
+	temp = array[x];
+	array[x] = array[y];
+	array[y] = temp;
 }
